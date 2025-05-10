4e0210c1b2277e1e5c59bb2e8eff4f9222aa17e9
NilsD <nils.dodenhoff@gmail.com>
2025-05-10 12:03:24 +0200

    debug db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'drizzle-kit';
 
 if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-//if (process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set:' + process.env.DATABASE_URL);
+if (process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set:' + process.env.DATABASE_URL);
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
